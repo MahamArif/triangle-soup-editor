f095fae49682c6cc6ef65f6c0bf9f2f8e3c9d273
Maham Arif <mahamarif49@gmail.com>
2023-04-18 01:09:27 -0400

    feat: code formatting added

--- src/RasterViewer.cpp
+++ src/RasterViewer.cpp
@@ -16,50 +16,51 @@ int main(int argc, char *args[])
     int width = 500;
     int height = 500;
     // The Framebuffer storing the image rendered by the rasterizer
-	Eigen::Matrix<FrameBufferAttributes,Eigen::Dynamic,Eigen::Dynamic> frameBuffer(width, height);
+    Eigen::Matrix<FrameBufferAttributes, Eigen::Dynamic, Eigen::Dynamic> frameBuffer(width, height);
 
-	// Global Constants (empty in this example)
-	UniformAttributes uniform;
+    // Global Constants (empty in this example)
+    UniformAttributes uniform;
 
-	// Basic rasterization program
-	Program program;
+    // Basic rasterization program
+    Program program;
 
-	// The vertex shader is the identity
-	program.VertexShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
-	{
-		return va;
-	};
+    // The vertex shader is the identity
+    program.VertexShader = [](const VertexAttributes &va, const UniformAttributes &uniform)
+    {
+        return va;
+    };
 
-	// The fragment shader uses a fixed color
-	program.FragmentShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
-	{
-		return FragmentAttributes(va.color(0),va.color(1),va.color(2));
-	};
+    // The fragment shader uses a fixed color
+    program.FragmentShader = [](const VertexAttributes &va, const UniformAttributes &uniform)
+    {
+        return FragmentAttributes(va.color(0), va.color(1), va.color(2));
+    };
 
-	// The blending shader converts colors between 0 and 1 to uint8
-	program.BlendingShader = [](const FragmentAttributes& fa, const FrameBufferAttributes& previous)
-	{
-		return FrameBufferAttributes(fa.color[0]*255,fa.color[1]*255,fa.color[2]*255,fa.color[3]*255);
-	};
+    // The blending shader converts colors between 0 and 1 to uint8
+    program.BlendingShader = [](const FragmentAttributes &fa, const FrameBufferAttributes &previous)
+    {
+        return FrameBufferAttributes(fa.color[0] * 255, fa.color[1] * 255, fa.color[2] * 255, fa.color[3] * 255);
+    };
 
-	// One triangle in the center of the screen
-	std::vector<VertexAttributes> vertices;
-	vertices.push_back(VertexAttributes(-1,-1,0));
-	vertices.push_back(VertexAttributes(1,-1,0));
-	vertices.push_back(VertexAttributes(0,1,0));
-    vertices[0].color << 1,0,0,1;
-    vertices[1].color << 0,1,0,1;
-    vertices[2].color << 0,0,1,1;
+    // One triangle in the center of the screen
+    std::vector<VertexAttributes> vertices;
+    vertices.push_back(VertexAttributes(-1, -1, 0));
+    vertices.push_back(VertexAttributes(1, -1, 0));
+    vertices.push_back(VertexAttributes(0, 1, 0));
+    vertices[0].color << 1, 0, 0, 1;
+    vertices[1].color << 0, 1, 0, 1;
+    vertices[2].color << 0, 0, 1, 1;
 
     // Initialize the viewer and the corresponding callbacks
     SDLViewer viewer;
     viewer.init("Viewer Example", width, height);
 
-    viewer.mouse_move = [](int x, int y, int xrel, int yrel){
+    viewer.mouse_move = [](int x, int y, int xrel, int yrel) {
     };
 
-    viewer.mouse_pressed = [&](int x, int y, bool is_pressed, int button, int clicks) {
-        vertices[2].position << (double(x)/double(width) * 2) - 1, (double(height-1-y)/double(height) * 2) - 1, 0, 1;
+    viewer.mouse_pressed = [&](int x, int y, bool is_pressed, int button, int clicks)
+    {
+        vertices[2].position << (double(x) / double(width) * 2) - 1, (double(height - 1 - y) / double(height) * 2) - 1, 0, 1;
         viewer.redraw_next = true;
     };
 
@@ -69,13 +70,14 @@ int main(int argc, char *args[])
     viewer.key_pressed = [&](char key, bool is_pressed, int modifier, int repeat) {
     };
 
-    viewer.redraw = [&](SDLViewer &viewer) {
+    viewer.redraw = [&](SDLViewer &viewer)
+    {
         // Clear the framebuffer
-        for (unsigned i=0;i<frameBuffer.rows();i++)
-            for (unsigned j=0;j<frameBuffer.cols();j++)
-                frameBuffer(i,j).color << 0,0,0,1;
+        for (unsigned i = 0; i < frameBuffer.rows(); i++)
+            for (unsigned j = 0; j < frameBuffer.cols(); j++)
+                frameBuffer(i, j).color << 0, 0, 0, 1;
 
-       	rasterize_triangles(program,uniform,vertices,frameBuffer);
+        rasterize_triangles(program, uniform, vertices, frameBuffer);
 
         // Buffer for exchanging data between rasterizer and sdl viewer
         Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic> R(width, height);
@@ -83,14 +85,14 @@ int main(int argc, char *args[])
         Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic> B(width, height);
         Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic> A(width, height);
 
-        for (unsigned i=0; i<frameBuffer.rows();i++)
+        for (unsigned i = 0; i < frameBuffer.rows(); i++)
         {
-            for (unsigned j=0; j<frameBuffer.cols();j++)
+            for (unsigned j = 0; j < frameBuffer.cols(); j++)
             {
-                R(i,frameBuffer.cols()-1-j) = frameBuffer(i,j).color(0);
-                G(i,frameBuffer.cols()-1-j) = frameBuffer(i,j).color(1);
-                B(i,frameBuffer.cols()-1-j) = frameBuffer(i,j).color(2);
-                A(i,frameBuffer.cols()-1-j) = frameBuffer(i,j).color(3);
+                R(i, frameBuffer.cols() - 1 - j) = frameBuffer(i, j).color(0);
+                G(i, frameBuffer.cols() - 1 - j) = frameBuffer(i, j).color(1);
+                B(i, frameBuffer.cols() - 1 - j) = frameBuffer(i, j).color(2);
+                A(i, frameBuffer.cols() - 1 - j) = frameBuffer(i, j).color(3);
             }
         }
         viewer.draw_image(R, G, B, A);
